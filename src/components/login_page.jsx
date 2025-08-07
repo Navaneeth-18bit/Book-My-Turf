c9591b4b809d5735a54e8a153818626e1f4f0bc4
Navaneeth Raj <navaneeth120405@gmail.com>
2025-08-07 23:34:38 +0530

    Add password visibility toggle and fix minor text errors in login page

--- src/components/login_page.jsx
+++ src/components/login_page.jsx
@@ -2,30 +2,48 @@ import React from 'react';
 
 function login_page()
 {
+    const togglePassword = () => {
+        const passwordField = document.getElementById('password')
+        const checkbox = document.getElementById('showPassword')
+
+        if (checkbox.checked)
+        {
+            passwordField.type = "text"
+
+        }
+        else
+        {
+            passwordField.type = "password"
+        }
+    }
+
+
+
     return(
         <>
         <div className='login_page'>
             <div className='login_page_greeting'>
                 <h1>Hi Welcome Back!</h1>
-                <p>Enetr your details to continue</p>
+                <p>Enter your details to continue</p>
             </div>
 
+            <hr />
             <div className='login_page_form'>
                 <form action="onsubmit">
                     <div className='login_page_form_input'>
                         <div>
-                            <label htmlFor="email">Email: </label>
-                            <input type="text" id='email' placeholder='Enter your username' />
+                            <label For="email">Email: </label>
+                            <input type="text" id='email' placeholder='Enter your username' required />
                         </div>
                         <div>
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="" id="" placeholder='Enter your password' required/>   
+                            <label For="password">Password</label>
+                            <input type="password" name="" id="password" placeholder='Enter your password' required/>   
                         </div>
                         
                         
                         <div>
-                            <label htmlFor="checkbox">Show password</label>
-                            <input type="checkbox"></input>
+                            <label For="showPassword">Show password</label>
+                            <input type="checkbox" id='showPassword' onClick={togglePassword}></input>
                         </div>
                         
                         <div>
