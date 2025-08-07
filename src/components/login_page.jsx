a79c27817a03d45ce0d5ed6f957b672cbe8c69a1
Navaneeth Raj <navaneeth120405@gmail.com>
2025-08-07 22:42:48 +0530

    Refactor login page structure and add basic CSS styles

--- src/components/login_page.jsx
+++ src/components/login_page.jsx
@@ -5,16 +5,24 @@ function login_page()
     return(
         <>
         <div className='login_page'>
-            <div>
+            <div className='login_page_greeting'>
                 <h1>Hi Welcome Back!</h1>
                 <p>Enetr your details to continue</p>
             </div>
 
-            <div>
-                <input type="text" name="" id="" placeholder='Enter your Username' />
-                <input type="password" name="" id="" placeholder='Enter your password' />
-                Show Password<input type="checkbox" name="" id="" />
-                <button>Login</button>
+            <div className='login_page_form'>
+                <form action="onsubmit">
+                    <div className='login_page_form_input'>
+                        <label htmlFor="email">Email: </label>
+                        <input type="text" id='email' placeholder='Enter your username' />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="" id="" placeholder='Enter your password' required/>
+                        <label htmlFor="checkbox">Show password</label>
+                        <input type="checkbox"></input>
+                        <button>Login</button>
+                    </div>
+                </form>
+                
                 <a href="">Forget Password</a>
             </div>
         </div>
